2eadf71a3081315762b878fd3c17a8b9b9e6ab68
Sadat Abdulla Al Muniem <sadatjnu9@gmail.com>
2019-01-28 08:50:43 +0600

    Add files via upload

---
+++ AnimationApps/Controllers/AnimationController.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AnimationApps.Controllers
+{
+    public class AnimationController : Controller
+    {
+        //
+        // GET: /Animation/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ShadowBox()
+        {
+            return View();
+        }
+
+        public ActionResult Color()
+        {
+            return View();
+        }
+
+        public ActionResult TypeText()
+        {
+            return View();
+        }
+
+        public ActionResult TypeTextShadow()
+        {
+            return View();
+        }
+
+        public ActionResult HelloWorld()
+        {
+            return View();
+        }
+
+        public ActionResult ImageSlide()
+        {
+            return View();
+        }
+
+        public ActionResult LetterBall()
+        {
+            return View();
+        }
+
+        public ActionResult MultipleEffects()
+        {
+            return View();
+        }
+
+        public ActionResult Parallax()
+        {
+            return View();
+        }
+
+        public ActionResult ScrollAnimation()
+        {
+            return View();
+        }
+
+         public ActionResult FlyingCircle()
+        {
+            return View();
+        }
+
+        public ActionResult Paroller()
+        {
+            return View();
+        }
+
+        public ActionResult CanvasAnimation()
+        {
+            return View();
+        }
+
+        public ActionResult Christmas()
+        {
+            return View();
+        }       
+
+        public ActionResult Finish()
+        {
+            return View();
+        }
+
+        public ActionResult Accordion()
+        {
+            return View();
+        }
+
+        public ActionResult Navbar()
+        {
+            return View();
+        }
+
+        public ActionResult MenuBar()
+        {
+            return View();
+        }
+
+        public ActionResult MenuBars()
+        {
+            return View();
+        }
+
+        public ActionResult EditableTable()
+        {
+            return View();
+        }
+
+        public ActionResult Clock()
+        {
+            return View();
+        }
+
+        public ActionResult ThreeDMenuBar()
+        {
+            return View();
+        }
+
+        public ActionResult ClipPath()
+        {
+            return View();
+        }
+
+        public ActionResult ButtonsLink()
+        {
+            return View();
+        }
+
+        public ActionResult DockMenu()
+        {
+            return View();
+        }
+
+        public ActionResult Loading()
+        {
+            return View();
+        }
+
+        public ActionResult SideNav()
+        {
+            return View();
+        }
+
+        public ActionResult FormBox()
+        {
+            return View();
+        }
+
+        public ActionResult Navigation()
+        {
+            return View();
+        }
+
+        public ActionResult FlipImage()
+        {
+            return View();
+        }
+
+        public ActionResult ColumnFilter()
+        {
+            return View();
+        }
+
+        public ActionResult PieMenu()
+        {
+            return View();
+        }
+
+        public ActionResult CircleMenu()
+        {
+            return View();
+        }
+
+        public ActionResult MobiMenu()
+        {
+            return View();
+        }
+
+        public ActionResult MenuFlip()
+        {
+            return View();
+        }
+
+        public ActionResult Sidebar()
+        {
+            return View();
+        }
+
+        public ActionResult CanvasMenu()
+        {
+            return View();
+        }
+
+        public ActionResult RadialMenu()
+        {
+            return View();
+        }
+
+        public ActionResult DropdownMenu()
+        {
+            return View();
+        }
+	}
+}
